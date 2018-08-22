585bcaa65e9b36a0c33e3ae32199be8f816166d6
Montse2018 <anmmtz@outlook.com>
2018-08-22 15:31:26 -0500

    ingresando api

---
+++ js/app.js
@@ -0,0 +1,21 @@
+const api = 'http://www.omdbapi.com/?apikey=82b3209c&s=Game+of+Thrones&plot=full&type=series'
+
+
+window.getMovies = (api) => {
+    console.log('api');
+    console.log(api);
+    fetch(api)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
+}
+
+/*getMovies("82b3209c", "Game of Thrones")
+.then(function (response) {
+    console.log(response);
+    let title = response.Title;
+    let image = response.Poster;
+    let year = response.Year;
+})*/
+
---
+++ js/main.js
@@ -0,0 +1,8 @@
+
+
+rendertInfo = (data) => {
+    document.getElementById("movies").addEventListener("click", event => {
+        let result = '';
+        const 
+    })
+}
